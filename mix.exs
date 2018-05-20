4d34c4463807001c74219e614e008094e17a20cd
Kosmas Chatzimichalis <kosmas@mach7x.com>
2018-05-20 18:01:00 +0200

    Add absinthe

--- mix.exs
+++ mix.exs
@@ -35,12 +35,16 @@ defmodule GraphqlApi.Mixfile do
     [
       {:phoenix, "~> 1.3.2"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
+      {:phoenix_ecto, "~> 3.3.0"},
       {:mariaex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.4.12"},
+      {:absinthe_plug, "~> 1.4.4"},
+      {:absinthe_phoenix, "~> 1.4.2"},
+      {:absinthe_relay, "~> 1.4.3"},
     ]
   end
 
